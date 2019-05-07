c6ca1a000862a603163968bb5d258c01c7d0f8e9
audrey mertens <amertens@worldofdigits.com>
2019-05-07 15:21:31 +0200

    front-page web and responsive done

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@ Template Name: front-page
     <div class="one_containeir_title">
         <h1>Innovative Analytics to empower Business.</h1>
         <p>As Data & Analytics enthusiasts, we are the architects of an emerging data-driven world.</p>
-        <button>En savoir plus sur BGFi</button>
+        <button class="btn">En savoir plus sur BGFi</button>
         <img  class="kenburns-top " src="<?php echo get_bloginfo('template_url') ?>/assets/images/Asset1.svg" alt="logo">
     </div>
     <div class="one_container_scroll">
@@ -35,6 +35,45 @@ Template Name: front-page
         <p>L'innovation, l'évolution et la collaboration sont les piliers de notre expérience collaborateur. Nous avons créé l'approche, les outils et les évènements nous permettant d'échanger, de découvrir, et de progresser ensemble.</p>
     </div>
 
+</section>
+
+<section class="three">
+    <div class="three_container">
+        <h1>Surfez sur l'actualité de BGFi</h1>
+        <p>Avec l'esprit collaboratif qui nous anime, nous partageons nos découvertes, nos réflexions, nos expertises mais aussi le quotidien de nos collaborateurs et partenaires. Explorez notre actualité ainsi que notre blog pour pour découvrir les dernières tendances d'un monde Data-driven.</p>
+    </div>
+</section>
+
+<section class="four">
+    <div class="four_container">
+        <div class="four_container_box">
+            <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/tunnel.png" alt="tunnel">
+            <h1>Nouveau Webinar Self-Service Analytics</h1>
+            <button class="btn">Lire plus</button>
+        </div>
+        <div class="four_container_box">
+        <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/workshop.png" alt="workshop">
+            <h1>Les 10 tendances Analytics de 2019</h1>
+            <button class="btn">Lire plus</button>
+        </div>
+    </div>
+</section>
+
+<section class="five">
+        <div class="five_container_box1">
+            <p>ils nous font confiance</p>
+        </div>
+        <div class="five_container_box2">
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/lvmh.png" alt="logo"></div>
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/bic.png" alt="logo"></div>
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/schweppes.png" alt="logo"></div>
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/dior.png" alt="logo"></div>
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/ibm.png" alt="logo"></div>
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/simone.png" alt="logo"></div>
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/detat.png" alt="logo"></div>
+            <div class="logos"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/effia.png" alt="logo"></div>
+
+        </div>
 </section>
 
 <?php get_footer(); ?>
